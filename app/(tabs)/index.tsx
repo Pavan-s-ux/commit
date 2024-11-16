05826f5cf842ea8b99c3c4b46c2147ec281fa93b
Pavan-s-ux <pavans232004@gmail.com>
2024-11-17 03:34:25 +0530

    app frontend commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,34 @@
-import { Stack } from 'expo-router';
-import { StyleSheet, View } from 'react-native';
-
-
-
+import { Link, Stack } from 'expo-router';
+import { StyleSheet, View ,FlatList, Pressable} from 'react-native';
+import{Image} from'expo-image';
+import { useMedia } from '~/Providers/Mediaprovider';
 export default function Home() {
+  const{assets,loadlocalAssets} = useMedia();
   return (
     <>
       <Stack.Screen options={{ title: 'Photos' }} />
-      
+      <View style={{ flex: 1 }}>
+      <FlatList data={assets}
+      numColumns={4}
+      keyExtractor={(item, index) => `${item.id}-${index}`} 
+      columnWrapperStyle={{gap:2}}
+      contentContainerStyle={{gap:2}}
+      onEndReached={() => {
+        console.log('End reached!');
+        loadlocalAssets(); 
+      }}
+      onEndReachedThreshold={0.9}
+       renderItem={({item}) => (
+        <Link href={`/asset?id=${item.id}`} asChild>
+          <Pressable style={{width:'25%'}}>
+       <Image source={{uri: item.uri}} style={{width:'100%' , aspectRatio:1}}/>
+       </Pressable>
+       </Link>
+  )}/></View>
     </>
+    
   );
+   
 }
 
 
